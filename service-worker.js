bb7065670f65c834987b6e6b7e9f3016da35a5db
Sartaj <sartaj@sartaj.me>
2024-08-12 16:20:20 +0000

    fix sw register

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const __dirname = self.location.pathname.replace("/service-worker.js", "");
 const relativePath = (p) => [__dirname, p].join("");
 
-const VERSION = "202408122";
+const VERSION = "202408123";
 
 importScripts(relativePath("/modules/service-workers/offline.sw.js"));
 importScripts(relativePath("/modules/service-workers/version.sw.js"));
