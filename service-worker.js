c82952b5375f8302b80bc177bbf28dc79cd5adcf
Sartaj <sartaj@sartaj.me>
2024-08-15 20:49:09 -0500

    Update version

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const __dirname = self.location.pathname.replace("/service-worker.js", "");
 const relativePath = (p) => [__dirname, p].join("");
 
 // const VERSION = `${Date.now()}`;
-const VERSION = "20240814.4";
+const VERSION = "20240815.1";
 
 importScripts(relativePath("/modules/service-workers/offline.sw.js"));
 importScripts(relativePath("/modules/service-workers/version.sw.js"));
