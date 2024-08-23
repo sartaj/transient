0b86f118407eb80065e5d14d8765a21b73613cb6
Sartaj <sartaj@sartaj.me>
2024-08-23 09:59:40 +0000

    move icons

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const __dirname = self.location.pathname.replace("/service-worker.js", "");
 const relativePath = (p) => [__dirname, p].join("");
 
 // const VERSION = `${Date.now()}`;
-const VERSION = "20240823.1";
+const VERSION = "20240823.2";
 
 importScripts(relativePath("/modules/service-workers/offline.sw.js"));
 importScripts(relativePath("/modules/service-workers/version.sw.js"));
